68b291640bb171af7eec5c46334cb8ef14fff051
Onsasepb <brianmurimi120@gmail.com>
2024-03-11 16:07:19 +0300

    initial commit

---
+++ src/introduction.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Aja")
+    println(20)
+    println(5+6)
+    print("kotlin")
+    print("Hello there")
+
+}
---
+++ src/variables.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    var firstname = "Aja"
+    firstname = "Achan"
+    var lastname = "Manas"
+    val pi = 3.14
+    var x = " "
+
+    println(pi)
+    println(firstname +x+ lastname)
+    println("My firstname is"+" "+firstname + " "+".I study at emobilis.")
+}
